ad5c2fa9862fc9014dfc66eac10ca097357cbd2c
Whitomtit <whitomtit@gmail.com>
2024-02-13 13:28:07 +0200

    Implemented GetPatient and GetMe calls

--- go.mod
+++ go.mod
@@ -3,26 +3,29 @@ module github.com/TekClinic/Patients-MicroService
 go 1.22.0
 
 require (
-	github.com/TekClinic/Auth-MicroService/auth_protobuf v0.0.0-20240212131444-c6cd1d175812 // indirect
-	github.com/TekClinic/Patients-MicroService/patients_protobuf v0.0.0-20240212175221-1853ee11d63d // indirect
+	github.com/TekClinic/Patients-MicroService/patients_protobuf v0.0.0-20240213102719-0beabe302d87
+	github.com/uptrace/bun v1.1.17
+	github.com/uptrace/bun/dialect/pgdialect v1.1.17
+	github.com/uptrace/bun/driver/pgdriver v1.1.17
+	github.com/uptrace/bun/extra/bundebug v1.1.17
+	google.golang.org/grpc v1.61.0
+)
+
+require (
+	github.com/TekClinic/Auth-MicroService/auth_protobuf v0.0.0-20240213101105-46ef4b17d661 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
-	github.com/uptrace/bun v1.1.17 // indirect
-	github.com/uptrace/bun/dialect/pgdialect v1.1.17 // indirect
-	github.com/uptrace/bun/driver/pgdriver v1.1.17 // indirect
-	github.com/uptrace/bun/extra/bundebug v1.1.17 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
-	google.golang.org/grpc v1.61.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240205150955-31a09d347014 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	mellium.im/sasl v0.3.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	authpb "github.com/TekClinic/Auth-MicroService/auth_protobuf"
 	ppb "github.com/TekClinic/Patients-MicroService/patients_protobuf"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
@@ -11,12 +13,22 @@ import (
 	"github.com/uptrace/bun/extra/bundebug"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
+
+type Service struct {
+	host string
+	port string
+}
+
+func (s Service) getAddr() string {
+	return s.host + ":" + s.port
+}
 
 type Patient struct {
 	bun.BaseModel `bun:"table:patients,alias:p"`
@@ -27,36 +39,114 @@ type Patient struct {
 
 type patientsServer struct {
 	ppb.UnimplementedPatientsServiceServer
-	db *bun.DB
+	db   *bun.DB
+	port string
+	auth *Service
 }
 
 func (server patientsServer) GetPatient(ctx context.Context, req *ppb.PatientRequest) (*ppb.Patient, error) {
-	patient := new(Patient)
-	err := server.db.NewSelect().Model(patient).Where("user_id = ?", req.GetUserId()).Scan(ctx)
+	if req.GetUserId() == "" {
+		return nil, status.Error(codes.InvalidArgument, "user_id argument is missing")
+	}
+	// for now only patient can fetch its own data
+	requesterId, err := server.getUserId(ctx, req.GetToken())
 	if err != nil {
-		return nil, status.Error(codes.NotFound, "")
+		return nil, err
+	}
+	if requesterId != req.GetUserId() {
+		return nil, status.Error(codes.PermissionDenied, "Only patient can fetch its own data")
+	}
+
+	patient, err := server.fetchPatientById(ctx, req.GetUserId())
+	if err != nil {
+		return nil, err
 	}
 	return &ppb.Patient{UserId: patient.UserId, Name: patient.Name}, nil
 }
 
-func createConnector() (*pgdriver.Connector, error) {
-	addr, set := os.LookupEnv("DB_ADDR")
-	if !set {
-		return nil, errors.New("DB_ADDR environment variable is missing")
+func (server patientsServer) GetMe(ctx context.Context, req *ppb.MeRequest) (*ppb.Patient, error) {
+	requesterId, err := server.getUserId(ctx, req.GetToken())
+	if err != nil {
+		return nil, err
 	}
-	user, set := os.LookupEnv("DB_USER")
-	if !set {
-		return nil, errors.New("DB_USER environment variable is missing")
+	patient, err := server.fetchPatientById(ctx, requesterId)
+	if err != nil {
+		return nil, err
 	}
-	password, set := os.LookupEnv("DB_PASSWORD")
-	if !set {
-		return nil, errors.New("DB_PASSWORD environment variable is missing")
+	return &ppb.Patient{UserId: patient.UserId, Name: patient.Name}, nil
+}
+
+func (server patientsServer) fetchPatientById(ctx context.Context, userId string) (*Patient, error) {
+	patient := new(Patient)
+	err := server.db.NewSelect().Model(patient).Where("user_id = ?", userId).Scan(ctx)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "")
 	}
-	database, set := os.LookupEnv("DB_DATABASE")
-	if !set {
-		return nil, errors.New("DB_DATABASE environment variable is missing")
+	return patient, nil
+}
+
+func (server patientsServer) getUserId(ctx context.Context, token string) (string, error) {
+	conn, err := grpc.Dial(server.auth.getAddr(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Print(err)
+		return "", status.Error(codes.Internal, err.Error())
 	}
-	return pgdriver.NewConnector(
+	defer conn.Close()
+	client := authpb.NewAuthServiceClient(conn)
+	clientResponse, err := client.ValidateToken(ctx, &authpb.TokenRequest{Token: token})
+	return clientResponse.GetUserId(), err
+}
+
+func getRequiredEnv(key string) (string, error) {
+	value, set := os.LookupEnv(key)
+	if !set {
+		return "", errors.New(key + " environment variable is missing")
+	}
+	return value, nil
+}
+
+func getOptionalEnv(key string, def string) string {
+	value, set := os.LookupEnv(key)
+	if set {
+		return value
+	}
+	return def
+}
+
+func fetchServiceParameters(serviceName string) (*Service, error) {
+	host, err := getRequiredEnv(fmt.Sprintf("MS_%s_HOST", strings.ToUpper(serviceName)))
+	if err != nil {
+		return nil, err
+	}
+
+	port := getOptionalEnv(fmt.Sprintf("MS_%s_PORT", strings.ToUpper(serviceName)), "9090")
+	return &Service{host: host, port: port}, nil
+}
+
+func fetchServerParameters() (*patientsServer, error) {
+	addr, err := getRequiredEnv("DB_ADDR")
+	if err != nil {
+		return nil, err
+	}
+	user, err := getRequiredEnv("DB_USER")
+	if err != nil {
+		return nil, err
+	}
+	password, err := getRequiredEnv("DB_PASSWORD")
+	if err != nil {
+		return nil, err
+	}
+	database, err := getRequiredEnv("DB_DATABASE")
+	if err != nil {
+		return nil, err
+	}
+	port := getOptionalEnv("GRPC_PORT", "9090")
+	auth, err := fetchServiceParameters("auth")
+	if err != nil {
+		return nil, err
+	}
+	connector := pgdriver.NewConnector(
 		pgdriver.WithNetwork("tcp"),
 		pgdriver.WithAddr(addr),
 		pgdriver.WithUser(user),
@@ -64,38 +154,35 @@ func createConnector() (*pgdriver.Connector, error) {
 		pgdriver.WithDatabase(database),
 		pgdriver.WithApplicationName("patients"),
 		pgdriver.WithInsecure(true),
-	), nil
-}
-
-func main() {
-	connector, err := createConnector()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	)
 	db := bun.NewDB(sql.OpenDB(connector), pgdialect.New())
 	db.AddQueryHook(bundebug.NewQueryHook(
 		bundebug.WithVerbose(true),
 		bundebug.FromEnv("BUNDEBUG"),
 	))
-	db.NewCreateTable().IfNotExists().Model((*Patient)(nil)).Exec(context.Background())
+	return &patientsServer{db: db, port: port, auth: auth}, nil
+}
 
-	srv := grpc.NewServer()
-	ppb.RegisterPatientsServiceServer(srv, &patientsServer{db: db})
-
-	// Register reflection service on gRPC authServer.
-	reflection.Register(srv)
-
-	grpcPort, set := os.LookupEnv("GRPC_PORT")
-	if !set {
-		grpcPort = "9090"
+func main() {
+	service, err := fetchServerParameters()
+	if err != nil {
+		log.Fatal(err)
 	}
-	listen, err := net.Listen("tcp", ":"+grpcPort)
+
+	_, err = service.db.NewCreateTable().IfNotExists().Model((*Patient)(nil)).Exec(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listen, err := net.Listen("tcp", ":"+service.port)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	log.Println("Server listening on :" + grpcPort)
+	srv := grpc.NewServer()
+	ppb.RegisterPatientsServiceServer(srv, service)
+
+	log.Println("Server listening on :" + service.port)
 	if err := srv.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
