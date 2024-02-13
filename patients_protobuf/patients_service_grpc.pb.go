0beabe302d875254841637c5bcaff683a31b7582
Whitomtit <whitomtit@gmail.com>
2024-02-13 12:27:19 +0200

    Patients protocol updated. Added GetMe request

--- patients_protobuf/patients_service_grpc.pb.go
+++ patients_protobuf/patients_service_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PatientsServiceClient interface {
 	GetPatient(ctx context.Context, in *PatientRequest, opts ...grpc.CallOption) (*Patient, error)
+	GetMe(ctx context.Context, in *MeRequest, opts ...grpc.CallOption) (*Patient, error)
 }
 
 type patientsServiceClient struct {
@@ -42,11 +43,21 @@ func (c *patientsServiceClient) GetPatient(ctx context.Context, in *PatientReque
 	return out, nil
 }
 
+func (c *patientsServiceClient) GetMe(ctx context.Context, in *MeRequest, opts ...grpc.CallOption) (*Patient, error) {
+	out := new(Patient)
+	err := c.cc.Invoke(ctx, "/patients.PatientsService/GetMe", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PatientsServiceServer is the server API for PatientsService service.
 // All implementations must embed UnimplementedPatientsServiceServer
 // for forward compatibility
 type PatientsServiceServer interface {
 	GetPatient(context.Context, *PatientRequest) (*Patient, error)
+	GetMe(context.Context, *MeRequest) (*Patient, error)
 	mustEmbedUnimplementedPatientsServiceServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedPatientsServiceServer struct {
 
 func (UnimplementedPatientsServiceServer) GetPatient(context.Context, *PatientRequest) (*Patient, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetPatient not implemented")
+}
+func (UnimplementedPatientsServiceServer) GetMe(context.Context, *MeRequest) (*Patient, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMe not implemented")
 }
 func (UnimplementedPatientsServiceServer) mustEmbedUnimplementedPatientsServiceServer() {}
 
@@ -88,6 +102,24 @@ func _PatientsService_GetPatient_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _PatientsService_GetMe_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PatientsServiceServer).GetMe(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/patients.PatientsService/GetMe",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PatientsServiceServer).GetMe(ctx, req.(*MeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // PatientsService_ServiceDesc is the grpc.ServiceDesc for PatientsService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var PatientsService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetPatient",
 			Handler:    _PatientsService_GetPatient_Handler,
+		},
+		{
+			MethodName: "GetMe",
+			Handler:    _PatientsService_GetMe_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
